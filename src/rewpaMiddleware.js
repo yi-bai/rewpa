eea36b10bf4557e766f9ba88824316f082769b7b
Bai Yi <baiyi@team-lab.com>
2017-06-29 14:03:45 +0900

    change effect parameters

--- src/rewpaMiddleware.js
+++ src/rewpaMiddleware.js
@@ -1,4 +1,5 @@
 import formattedAction from './utils/formattedAction';
+import getPath from './utils/getPath';
 import _ from 'lodash';
 
 export default (rewpa) => ({ dispatch, getState }) => next => action => {
@@ -12,7 +13,11 @@ export default (rewpa) => ({ dispatch, getState }) => next => action => {
   if(prevState == nextState){
     const effectFunc = rewpa(nextState, _.assign({}, action, { type: '@@rewpa/GET_EFFECT_FUNC' }));
     if(_.isFunction(effectFunc)){
-      return effectFunc(action, dispatch, getState);
+      return effectFunc(
+        action,
+        function(_action){ return dispatch(_.assign({}, _action, { path: action.path })); },
+        function(){ return getPath(getState(), action.path) },
+        dispatch, getState);
     }else{
       console.log(`Warning: no state changed after action ${action.type}`);
       return null;
@@ -21,9 +26,15 @@ export default (rewpa) => ({ dispatch, getState }) => next => action => {
     const onChangePaths = rewpa(nextState, _.assign({}, action, { type: '@@rewpa/GET_ON_CHANGE_PATH', __type: 'GET_ON_CHANGE_PATH' }));
     for(const index in onChangePaths.list){
       if(onChangePaths.list[index]){
+        const thisActionPath = action.__path.slice(0, index).join('.');
         onChangePaths.list[index](
-          formattedAction({ type: `${action.__path.slice(0, index).join('.')}/_ON_CHANGE`, payload: { prevState }}),
-          dispatch, getState);
+          formattedAction(
+            {
+              type: `${thisActionPath}/_ON_CHANGE`,
+              payload: { prevState: getPath(prevState, thisActionPath), prevRootState: prevState }}),
+            function(_action){ return dispatch(_.assign({}, _action, { path: thisActionPath })); },
+            function(){ return getPath(getState(), thisActionPath) },
+            dispatch, getState);
       }
     }
     return result;
